14ad1460991317702629c8632c9a7bae41546061
BaldwIan <159124@student.vansd.org>
2019-06-28 19:09:06 -0700

    GUI window

--- main.cpp
+++ main.cpp
@@ -4,10 +4,17 @@
 
 int main(int argc, char *argv[])
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-	{
-		std::cout << "SDL failed to initialize. SDL Error : " << SDL_GetError() << std::endl;
-	}
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window *window = SDL_CreateWindow("SDL Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+	
+	SDL_Delay(3000);
 	
 	return EXIT_SUCCESS;
 }
